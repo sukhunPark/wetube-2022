f0879486cd8c437bb50ed91a55eeddf3ef98b87c
박석훈 <psh7452@naver.com>
2022-08-23 16:43:12 +0900

    3.0~3.4

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const  PORT = 4000;
+
+ const app = express();
+
+const handleHome =  (req, res) => {
+    return res.send("Yariyalloo yalralloo");
+};
+
+const handleLogin = (req,res) =>{
+    return res.send("Login here");
+};
+
+app.get("/", handleHome); 
+app.get("/login", handleLogin);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT,handleListening);
